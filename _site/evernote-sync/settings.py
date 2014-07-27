a1d2543ec62008e5ffae4665301e3fc4eeb41f11
anderson <novohust@163.com>
2014-07-27 12:35:26 +0800

    deploy

---
+++ _site/evernote-sync/settings.py
@@ -0,0 +1,82 @@
+# coding=utf-8
+
+noteList = {
+    '并发' : [
+    	# java 并发基本知识
+    	'https://www.evernote.com/shard/s133/sh/23a4cd7a-a036-4525-aed5-b027253ab6fe/859bebf08659ac7bf97062157a69a1b2',
+    	# JMM
+    	'https://www.evernote.com/shard/s133/sh/17b41966-c5fd-4dcc-af9c-e21738ab5501/2e30a11bbfa9fb62a76c2a2af52b9ad0',
+    	# AQS & 高级同步器
+    	'https://www.evernote.com/shard/s133/sh/1ab39e5a-ec6e-4989-a11b-cdada5482a47/05664ff0a35c95797b3477aab94ca971',
+    	# JUC 中的并发集合
+    	'https://www.evernote.com/shard/s133/sh/fb455d08-e466-4575-80da-4959dfa7caf3/dd353026a3a363a7d5fd6137e021e06a',
+    	# Executor 之 线程池 / 定时器
+    	'https://www.evernote.com/shard/s133/sh/a1359513-c482-4e3c-a6e6-ae8c5c1e72f4/696c794f2c63bc621758e5f1ac5b9e97',
+    	# Lock-free 算法
+    	'https://www.evernote.com/shard/s133/sh/032b4f82-0b1d-430a-8538-b266243bb4dd/b7fdad034f43d6b2b261024ca9ad614d',
+    	# 定时器
+    	'https://www.evernote.com/shard/s133/sh/d227e30b-0728-4599-9556-12c9cab77ab3/6a4d4d5c0608363acc46773d5aa27a76',
+    ],
+    '算法' : [
+    	# 贝叶斯
+        'https://www.evernote.com/shard/s133/sh/76de59d3-0737-4820-9b4f-27b0d8bdbd87/3e5d57b00bf6aa98463ad27e5152ff72',
+        # 堆
+        'https://www.evernote.com/shard/s133/sh/d0610154-c631-45a4-be3b-46c8ab4f2799/6614170b17c641f3f810b2d6968e184a',
+        # LCA 和 RMQ
+        'https://www.evernote.com/shard/s133/sh/2c6e561b-f7b5-4855-8f6a-389a838a7108/4eb948f7ee01dac3c30347524825d3e6',
+        # 非递归的二叉树遍历
+        'https://www.evernote.com/shard/s133/sh/f211cc0b-631f-46b9-a583-f835fe3a26fa/338f5395301e8f9371facaabfa000498',
+        # 并查集
+        'https://www.evernote.com/shard/s133/sh/1e4ce1b1-2318-4243-bb53-162c0d148ab6/1c788860444ecae74892f5cc7b517cdd',
+        # 二进制索引树(树状数组)
+        'https://www.evernote.com/shard/s133/sh/e7b9eaf6-4d9a-46b2-b2dd-a580b9efbe8d/719360ec5b6f9d86698daf663cfd919e',
+        # 线段树
+        'https://www.evernote.com/shard/s133/sh/dd8a499d-db7d-45bf-b903-1952d470344c/f09244ac0679d3d20f2fe9d2b60f3e54',
+        # 桶排序的应用
+        'https://www.evernote.com/shard/s133/sh/d20f37ab-3690-4311-98dd-4ec481f4b23e/d2690da69b1e1753a5deafbd50a2e7a1',
+        # 背包问题
+        'https://www.evernote.com/shard/s133/sh/d76f86c4-1ced-4491-a1df-88e898f9883f/b168fdd8dcff1573e6c42d1758302be7',
+        # 回溯
+        'https://www.evernote.com/shard/s133/sh/65f61b9c-550a-4766-9ca8-9096cb67617e/7eca1b119feaeca2424994615125ab32',
+        # kmp
+        'https://www.evernote.com/shard/s133/sh/c080ba1b-77e9-46fb-a48c-d9a372b412f2/6dcb405f3c8bc0f26e66300bbb016081',
+        # rabin-karp
+        'https://www.evernote.com/shard/s133/sh/8e679736-320d-4478-a8df-b872415bfdcb/adb7dc5f3409de988aa882bdb0b5d706'
+    ],
+    '数据库' : [
+    	# MYSQL
+    	'https://www.evernote.com/shard/s133/sh/d308cb41-961f-433d-ae84-98bc3015ed13/68ef39ac3bcb6b1fd5a72a8c657bacca',
+    ],
+    'cache' :[
+    	# HTTP cache
+    	'https://www.evernote.com/shard/s133/sh/64c6933a-bba9-4f09-a3bd-78cf0fc89fc4/5344939c3032ea84e9201f33163a2eac',
+    	# Cache 理论
+    	'https://www.evernote.com/shard/s133/sh/733916c8-35c2-44a8-bb82-ae94fde3aef3/6978a5c85e611f882b246fe408c15258',
+    ],
+    'c和os' :[
+    	# C & 虚拟内存
+    	'https://www.evernote.com/shard/s133/sh/47e67ddc-4bf4-43b3-a1ff-092bef12b46f/c3c4e93824b6977cedfe04b131124c31',
+    	# CPU Cache 与 存储器层次结构
+    	'https://www.evernote.com/shard/s133/sh/3bb6ac50-c660-49d9-b214-88b6357e35df/939f9b1f174859d1a917945d9e33db77',
+    	# Mode Switch 和 Context Switch
+    	'https://www.evernote.com/shard/s133/sh/dfd3dbfa-d52d-43f9-992e-1c33e50e004d/43e12d32cc6d4e7315f04432a80d7326',
+    	# 网卡中断负载均衡
+    	'https://www.evernote.com/shard/s133/sh/94d3673c-8b42-4ecc-8334-c35e82df3616/77845e48462533093d284a987b4d0bff'
+    ],
+    '网络' : [
+    	# TCP 复习
+    	'https://www.evernote.com/shard/s133/sh/9389230e-c3eb-415c-baa1-83114a8709c1/5715d770bd474dc0ae4460d15a3b7870',
+    	# 链路层 网络层 UDP IO模型
+    	'https://www.evernote.com/shard/s133/sh/66441092-3553-4aa0-92ab-7e6dff56fd6b/9d104928f2c0e463d29d80b621e15156',
+    	# Netty3 源码分析
+    	'https://www.evernote.com/shard/s133/sh/4f9f922c-6140-4865-94c8-6e86f3b94cc6/18f84c1fffc9ce58ccd9764c5a161024',
+    	# Netty 3/4 线程模型的区别
+    	'https://www.evernote.com/shard/s133/sh/c04ad94d-e6b1-4044-bd2d-b5bfa7522d0f/ae81a4b90ef29401d9678270d64eedeb',
+    	# RST/ java 网络异常
+    	'https://www.evernote.com/shard/s133/sh/1f29d7f2-dcac-4cce-b270-049942a4d14d/910bbdfaab7174430de656d897d22fd7',
+    	# 高性能网络通讯笔记
+    	'https://www.evernote.com/shard/s133/sh/db5e50f9-6e95-482e-9fc1-0402258c7169/d93f8456e248c46ee54651948785b13f'
+    ]
+}
+
+expire = 24 * 3600 # seconds, 1 day
